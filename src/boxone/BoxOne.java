72d0769f07faaf3d0f20c5034ace5de6742db89f
unknown <ST10266958@vcconnect.edu.za>
2023-10-09 10:27:41 +0000

    First Commit

---
+++ src/boxone/BoxOne.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boxone;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class BoxOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //program to show you different JOP Boxes
+        
+        JOptionPane.showMessageDialog(null,"Types of JOP Boxes");
+        
+        JOptionPane.showInputDialog(null,"Enter your name>>>>>","New Frame",JOptionPane.ERROR_MESSAGE);
+        
+        
+        
+        JOptionPane.showMessageDialog(null,"End\n Thank you");
+    }
+    
+}
